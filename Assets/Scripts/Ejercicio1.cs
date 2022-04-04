7c0f5d5ce170b40616260da559f72d60cc8898a2
OctavioSpano <tutispano@gmail.com>
2022-04-04 12:10:15 -0300

    Initial commit

---
+++ Assets/Scripts/Ejercicio1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ejercicio1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int EdadUsuario;
+    public Text miTexto;
+
+    void Start()
+    {
+        if (EdadUsuario >= 18)
+        {
+            miTexto.text = "Es mayor de edad"
+            Debug.Log("Es mayor de edad");
+        }
+        else
+        {
+            Debug.Log("No es mayor de edad");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
